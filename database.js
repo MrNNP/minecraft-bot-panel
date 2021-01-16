4e59bc32a7967836175b110c3e7d45659252e3f5
Naishadh Patel <65918272+MrNNP@users.noreply.github.com>
2021-01-15 22:08:36 -0500

    database save interval

--- database.js
+++ database.js
@@ -1,2 +1,19 @@
+const fs = require('fs');
 var database = require('./database.json');
+
+module.exports = ()=>{ return database;};
+module.exports.update = async function(db)
+{
+
+    database = db;  
+    writeToFile(db);
+
+};
+
+async function writeToFile(database){
+
+await fs.writeFileSync('./database.json',JSON.stringify(database));
+return;
+
+};
 
--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 const Discord = require('discord.js');
 const db = require('./database.js'); 
+
+var database = db();
+
+
+
+setInterval(()=>{db.update(database)},10000);
+
