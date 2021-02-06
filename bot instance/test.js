5d78813bbc1a520345dd887fcdde0714712b3080
Naishadh Patel <65918272+MrNNP@users.noreply.github.com>
2021-02-06 14:16:40 -0500

    reset

--- bot instance/test.js
+++ bot instance/test.js
@@ -1,4 +1,15 @@
-var yes = 'false';
+const { Client} = require('./new.js');
+var bot = new Client({output:(loga)=>{
+    console.log(loga)
+},quitSelf:(loga)=>{
+    console.log(loga)
+}},48409238409238,'edisonmc.mcs.lol',25565,'MrNNP',undefined,'MrNNP');
 
+bot.bot.on('spawn',()=>{
+    console.log('spawned');
+});
 
-console.log(yes ? yes:10);
+//setTimeout(()=>{
+//    bot.bot.quit();
+//    console.log('done');
+//},10000);
